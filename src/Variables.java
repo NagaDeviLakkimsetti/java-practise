ac966caeab05553b897246a49b8dcd3361386b5d
NagaDeviLakkimsetti <18q91a05k7_cse@mrce.in>
2025-04-11 07:21:10 +0000

    adding practice files

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables{
+    public static void main(String args[])
+    {
+        int a=5;
+        double b= 8.9;
+        char c='A';
+        boolean value=true;
+        String s="abc";
+        System.out.println("integer:" +a+ "double:" +b+ "char:" +c+ "boolean:" +value+ "string" +s);
+    }
+}
