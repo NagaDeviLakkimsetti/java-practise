510c7306e34c9dcaadf6aabb833cbd4f7b9f748c
NagaDevi Lakkimsetti <18q91a05k7_cse@mrce.in>
2025-04-11 17:11:10 +0000

    add the sum of variables file

---
+++ src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+    public static void main(String args[])
+    {
+        int a=10,b=20;
+        int sum=a+b;
+        System.out.println("Integer1:" +a+ "Integer2:" +b);
+        System.out.println("The sum is:" +sum);
+    }
+}
