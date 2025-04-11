6efda61d58be2ece52deadc53f8bcc97888b0121
NagaDevi Lakkimsetti <18q91a05k7_cse@mrce.in>
2025-04-11 17:38:25 +0000

    Even odd with input from user

---
+++ src/UserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class UserInput{
+    public static void main(String args[]){
+        Scanner input= new Scanner(System.in);
+        int a= input.nextInt();
+        if(a%2==0)
+        System.out.println(+a+ "is an even number");
+        else
+         System.out.println(+a+ "is an even number");
+
+    }
+}
