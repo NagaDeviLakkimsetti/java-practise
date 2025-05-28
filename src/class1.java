cc1d25e5bb41d81a96b3702b2a77c6809abdf4bc
NagaDevi Lakkimsetti <18q91a05k7_cse@mrce.in>
2025-05-28 20:16:24 +0000

    This is a sample program on classes in java

---
+++ src/class1.java
@@ -0,0 +1,12 @@
+public class class1{
+    String name ="Honda";
+    int num= 2008;
+
+    public void display(){
+        System.out.println("Name:"+name+ "number:"+num);
+    }
+    public static void main(String args[]){
+        class1 car=new class1();
+        car.display();
+    }
+}
